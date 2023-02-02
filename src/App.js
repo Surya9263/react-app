6321897b7b2a4efc7b53af92fe3024a362239518
Surya Srivastava <suryasrivastava724@gmail.com>
2023-02-02 22:11:36 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import "./App.css";
 function App() {
   function launchOneDrivePicker() {
     var odOptions = {
-      clientId: "INSERT-APP-ID-HERE",
+      clientId: "9c0d6026-4d2d-496a-b58a-c0de8ab3425a",
       action: "query",
       multiSelect: true,
       advanced: {},
       success: function (files) {
         /* success handler */
+        console.log("SUCCESS: ", files);
       },
       cancel: function () {
         /* cancel handler */
+        console.log("CANCELLED");
       },
       error: function (error) {
         /* error handler */
+        console.log("ERROR: ", error);
       },
     };
-    launchOneDrivePicker.open(odOptions);
+    OneDrive.open(odOptions);
   }
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button onClick={() => launchOneDrivePicker()}>
+        Click here
+      </button>
+    </div>
+  );
 }
 
 export default App;
